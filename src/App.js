0f8f791cf100a3a5b5025ed8d428d7e6405fb7bc
Giovane Forlenza <36609868+GiovaneForlenza@users.noreply.github.com>
2021-09-28 22:34:46 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import items from './data';
+import Categories from './Categories';
+import Menu from './Menu';
+
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems)
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+
+
+    </main>
+  );
+}
+
+export default App;
